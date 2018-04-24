0f4d5c1552447bcb102cb9d3942b2d3566f70296
ghost461 <lingmilch@sina.com>
2018-04-24 15:01:34 +0800

    add idt for irq & timer

--- init/entry.c
+++ init/entry.c
@@ -2,6 +2,7 @@
 #include"../include/debug.h"
 #include"../include/gdt.h"
 #include"../include/idt.h"
+#include"../include/timer.h"
 
 int kern_entry() {
 	init_debug();
@@ -11,8 +12,15 @@ int kern_entry() {
 	console_clear();
 	printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
 
+	init_timer(200);
+
+	// ¿ªÆôÖÐ¶Ï
+	asm volatile ("sti");
+
+	/*
 	asm volatile ("int $0x3");
 	asm volatile ("int $0x4");
+	*/
 
 	return 0;
 }
